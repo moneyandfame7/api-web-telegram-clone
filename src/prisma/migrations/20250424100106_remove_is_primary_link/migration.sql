74c330e05066c4071a9b7370effcb1dbc4369253
Davyd <davidoo1234e@gmail.com>
2025-04-24 12:01:31 +0200

    chore: remove 'isPrimary' field from invite link

---
+++ src/prisma/migrations/20250424100106_remove_is_primary_link/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isPrimary` on the `ChatInviteLink` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ChatInviteLink" DROP COLUMN "isPrimary";
